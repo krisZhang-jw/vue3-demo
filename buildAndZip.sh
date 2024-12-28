0087174f2533b0c2595b9e02a6cc2351c470f718
zhangjiewen <474350393@qq.com>
2024-12-28 22:50:16 +0800

    feat: buildAndZip-dist.zip改为目录名.zip && 暂停脚本让用户退出

--- buildAndZip.sh
+++ buildAndZip.sh
@@ -3,6 +3,14 @@
 # 获取操作系统类型
 OS_TYPE=$(uname)
 
+# 获取当前脚本所在目录的路径
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+
+# 获取目录名称
+DIR_NAME=$(basename "$SCRIPT_DIR")
+
+DIR_NAME="dist"
+
 # 运行 pnpm run build
 echo "Starting build..."
 npm i pnpm -g
@@ -18,15 +26,15 @@ if [ $? -eq 0 ]; then
     # 使用 zip 命令压缩 build 目录
     if [[ "$OS_TYPE" == "Darwin" ]]; then
         echo "当前系统是 macOS"
-        zip -r dist.zip dist/
+        zip -r $DIR_NAME.zip dist/
     elif [[ "$OS_TYPE" == "CYGWIN"* || "$OS_TYPE" == "MINGW"* ]]; then
         echo "当前系统是 Windows (通过 Cygwin 或 MinGW)"
-        # Compress-Archive -Path dist/ -DestinationPath dist.zip
-        # tar -czvf dist.tar.gz dist
-        7z a dist.zip dist
+        # Compress-Archive -Path dist/ -DestinationPath $DIR_NAME.zip
+        # tar -czvf $DIR_NAME.tar.gz dist
+        7z a $DIR_NAME.zip dist
     fi
     if [ $? -eq 0 ]; then
-      echo "ZIP file created successfully: dist.zip"
+      echo "ZIP file created successfully: $DIR_NAME.zip"
     else
       echo "Failed to create ZIP file."
     fi
@@ -36,3 +44,5 @@ if [ $? -eq 0 ]; then
 else
   echo "Build failed. Skipping ZIP creation."
 fi
+
+read -p "Press any key to exit..."
