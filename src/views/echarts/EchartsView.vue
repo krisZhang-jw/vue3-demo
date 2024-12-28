90b91bca50994abef189088d4f0b29cd486a79d6
zhangjiewen <474350393@qq.com>
2024-12-28 23:19:24 +0800

    feat: echarts基础结构

---
+++ src/views/echarts/EchartsView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="echarts-view">
+    <h1>Echarts</h1>
+    <div id="main" style="width: 600px; height: 400px"></div>
+  </div>
+</template>
+<script setup lang="ts"></script>
+<style lang="scss" scoped></style>
